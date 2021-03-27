34ca1e8667e4992d84b7fe87a71161e29c69e20d
Basanta Goswami <basantakumar.devgoswami@gmail.com>
2021-03-27 17:44:33 +0530

    Add logic to start server only after connecting to mongodb

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');         // Used for routing
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;      // Port either 5000 or environment variable from heroku
+const dotenv   = require('dotenv');
+const mongoose = require('mongoose');
 
-
-const espRoutes = require('./routes/esp.classify.js');
+// Add /esp/ routes
+const espRoutes = require('./routes/espRoutes.js');
 app.use('/esp', espRoutes);
 
 // Serve static assets / react build folder if we are in production, works when deployed to heroku
@@ -18,7 +20,20 @@ if(process.env.NODE_ENV === 'production') {
     });
 }
 
-// Start node server on PORT
-app.listen(port, () => {
-    console.log("Server started on port", port);
-});
+
+// Load variables from the .env file
+dotenv.config();
+
+// Connect to mongoDB cloud database
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    // When connection to db successful Start node server on port
+    .then(() => {
+        app.listen(port, () => {  console.log("Server started on port", port);  });
+    })
+    // If any err
+    .catch((error) => {  console.error(error.message);  }
+);
+
+// Not sure why, but just to avoid warnings I guess
+mongoose.set('useFindAndModify', false);
+
