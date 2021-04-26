e2aabc0dde57060de006a2cefef44eacfbd4eadf
Basanta Goswami <basantakumar.devgoswami@gmail.com>
2021-04-26 20:47:37 +0530

    Add login form to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,52 @@
+import React, {useState} from 'react';
+import Loginform from './components/LoginForm/LoginForm';
+
 function App() {
+  
+  // In production code, the username and passwords will be fetched from a database 
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user,setUser] = useState({email:""});
+  const [error,setError] = useState();
+
+  // Function that checks login details and updates the state of the app
+  // if login is successful
+  const Login = details => {
+    console.log("Logged in");
+    if(details.email===adminUser.email && details.password===adminUser.password){
+      console.log("Loggedin");
+      setUser({
+        email: details.email
+      });
+    }
+    else{
+      console.log("details dont match")
+      setError("details dont match")
+    } 
+  }
+
+  // Sets the state to logged out
+  const Logout = () => {
+    console.log("logout");
+    setUser({email:""})
+    setError("")
+  }
+
+
+  
   return (
-    <h1>App</h1>
+    <div className="App">
+      {(user.email!=="")?(
+        <div className="Menu">
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ):( 
+        <Loginform Login={Login} error ={error}/>
+      )}
+    </div>
   );
 }
 
---
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import './styles.css';
+
+export default function Loginform({Login, error}) {
+    const [details,setDetails] = useState({email:"",password:""});
+
+    const submitHandler = e =>{
+        e.preventDefault();
+        Login(details);
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-inner">
+                <h2>Login</h2>
+                {(error!=="")?(<div className="error">{error}</div>): ""}
+                <div className="form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input type="text" name="name" id="name" onChange={e=>setDetails({...details,email:e.target.value})} value={details.email}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" id="password" onChange={e=>setDetails({...details,password:e.target.value})} value={details.password}/>
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
+    )
+}
